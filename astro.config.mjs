bece190fdebc9e9bc4d307779337919238d232ea
Dusan Spasojevic <dusan.kostic37@gmail.com>
2025-11-17 10:58:59 +0100

    Enable vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,20 @@
 import { defineConfig } from "astro/config";
+import vercel from "@astrojs/vercel/server";
 import mdx from "@astrojs/mdx";
 import sitemap from "@astrojs/sitemap";
 import tailwind from "@astrojs/tailwind";
 import solidJs from "@astrojs/solid-js";
 
-// https://astro.build/config
 export default defineConfig({
   site: "https://www.seomissioncontrol.com",
+  output: "server",
+  adapter: vercel(),
+
   trailingSlash: "always",
   devToolbar: {
     enabled: false,
   },
+
   integrations: [
     mdx(),
     sitemap(),
